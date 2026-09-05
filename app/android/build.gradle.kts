5077fec7e871e3ba40bfc847e3467693c206e453
abhishekharogeri-gif <abhishekharogeri@gmail.com>
2026-09-05 18:15:53 +0530

    fix(android): set kotlin jvm target validation mode to warning

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -20,8 +20,8 @@ subprojects {
 }
 subprojects {
     tasks.withType<JavaCompile>().configureEach {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
     }
 }
 
